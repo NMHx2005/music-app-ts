ce25eb0a14decb6b79b8148f3013161fc9803d7a
Nguyen Manh Hung <22092005nguyenhung@gmail.com>
2024-10-01 12:37:57 +0700

    Làm trang danh sách bài hát (bên client)

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -1,7 +1,10 @@
 import { Express } from "express";
 import { topicRoute } from "./topic.route";
+import { songRoute } from "./song.route";
 
 
 export const routeClient = (app: Express) => {
     app.use("/topics", topicRoute);
+
+    app.use("/songs", songRoute);
 }
